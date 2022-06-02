0612c507422a014fa0d5fae79389ad54c81dd120
Mai-San <74722819+LightNickCZ@users.noreply.github.com>
2022-06-02 14:55:07 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    pocitadlo += -1
+    basic.showNumber(pocitadlo)
+})
 input.onButtonPressed(Button.B, function () {
     pocitadlo += 1
     basic.showNumber(pocitadlo)
